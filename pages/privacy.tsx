f83755778735f639c08901e2c43483103a9f4afe
Miha Žnidar <mayki.znidar@gmail.com>
2023-03-11 17:21:04 +0100

    Privacy page

---
+++ pages/privacy.tsx
@@ -0,0 +1,16 @@
+import Layout from '../components/Layout';
+import { Navbar } from '../components/Navbar';
+import { Privacy } from '../components/Privacy';
+import { Footer } from '../components/Footer';
+
+
+
+const PrivacyPage = () => (
+  <Layout>
+    <Navbar />
+    <Privacy />
+    <Footer />
+  </Layout>
+)
+
+export default PrivacyPage
