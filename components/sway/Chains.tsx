c2061388562cec62745edcd6cdda191520d0230d
Miha Žnidar <mayki.znidar@gmail.com>
2023-03-11 17:22:27 +0100

    Styles changes

--- components/sway/Chains.tsx
+++ components/sway/Chains.tsx
@@ -2,7 +2,7 @@ export const Chains = () => (
   <section className='sway-chains-section'>
     <div className='container'>
       <div className='row g-lg-5'>
-        <div className='col-12 col-lg-6 border'>
+        <div className='col-12 col-lg-6 mx-auto'>
           <div className='offset-4 col-8 offset-md-2 col-md-10 py-3'>
             <img src='/assets/sway/Polygon.png' alt='Polygon logo' />
           </div>
@@ -26,27 +26,7 @@ export const Chains = () => (
             </div>
           </div>
         </div>
-        <div className='col-12 col-lg-6'>
-          <div className='offset-4 col-8 offset-md-2 col-md-10 py-3'>
-            <img src='/assets/sway/Hedera.png' alt='Hedera logo' />
-          </div>
-          <div className='row table g-1'>
-            <div className='col-4 col-md-2 p-3'>
-              <p>Launched</p>
-              <p>Supply</p>
-              <p>Symbol</p>
-              <p>Contract</p>
-
-            </div>
-            <div className='col-8 col-md-10 p-3 greenBackground'>
-              <p>2023 (TBD)</p>
-              <p>TBD</p>
-              <p>SWAY, SWAY[hts]</p>
-              <p>TBD</p>
-              <p>&nbsp;</p>
-            </div>
-          </div>
-        </div>
+  
       </div>
     </div>
   </section>
